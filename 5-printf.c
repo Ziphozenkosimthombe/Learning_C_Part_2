bb505f3e97d40ef6034d50dd41c1eabfd0a8d856
ziphozenkosimthombe <ziphoncayiyana@gmail.com>
2025-02-13 04:42:53 +0200

    C program that the exactly given text

---
+++ 5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * C program that print exactly the text given.
+ * Return: 0
+ */
+
+int main(void) {
+  printf("with proper grammar, but the outcome is a piece of art,\n");
+  return (0);
+}
